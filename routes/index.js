b1ac5e74e8b598cae4d59f55a0473f31acbebe58
SunilSigar <sunilsigar001@gmail.com>
2017-12-11 21:49:14 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,28 @@ var mongo = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
-var url = 'mongodb://sunilsigar:vfr4m5tgb@ds135946.mlab.com:35946/jokes';
-// var url = 'mongodb://localhost:27017/bestjokes';
+// var url = 'mongodb://sunilsigar:vfr4m5tgb@ds135946.mlab.com:35946/jokes';
+var url = 'mongodb://localhost:27017/bestjokes';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index');
+// });
 
-router.get('/get-data', function(req, res, next) {
-  var resultArray = [];
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    var cursor = db.collection('userdata').find();
-    cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      resultArray.push(doc);
-    }, function() {
-      db.close();
-      res.render('index', {items: resultArray});
-    });
-  });
-});
+// router.get('/get-data', function(req, res, next) {
+//   var resultArray = [];
+//   mongo.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     var cursor = db.collection('userdata').find();
+//     cursor.forEach(function(doc, err) {
+//       assert.equal(null, err);
+//       resultArray.push(doc);
+//     }, function() {
+//       db.close();
+//       res.render('index', {items: resultArray});
+//     });
+//   });
+// });
 
 router.post('/insert', function(req, res, next) {
   var item = {
@@ -44,7 +44,20 @@ router.post('/insert', function(req, res, next) {
     });
   });
 
-  res.redirect('/');
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('userdata').find().sort({$natural:-1});
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+
+  // res.redirect('/');
 });
 
 // router.post('/update', function(req, res, next) {
