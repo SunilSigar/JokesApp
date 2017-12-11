73fd1b92395c16931eda025af271490739259d01
SunilSigar <sunilsigar001@gmail.com>
2017-12-11 21:40:50 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 var url = 'mongodb://sunilsigar:vfr4m5tgb@ds135946.mlab.com:35946/jokes';
+// var url = 'mongodb://localhost:27017/bestjokes';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,7 +16,7 @@ router.get('/get-data', function(req, res, next) {
   var resultArray = [];
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
-    var cursor = db.collection('user-data').find();
+    var cursor = db.collection('userdata').find();
     cursor.forEach(function(doc, err) {
       assert.equal(null, err);
       resultArray.push(doc);
@@ -28,14 +29,15 @@ router.get('/get-data', function(req, res, next) {
 
 router.post('/insert', function(req, res, next) {
   var item = {
-    title: req.body.title,
+    // title: req.body.title,
     content: req.body.content,
-    author: req.body.author
+    // author: req.body.author,
+    category:req.body.category
   };
 
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
-    db.collection('user-data').insertOne(item, function(err, result) {
+    db.collection('userdata').insertOne(item, function(err, result) {
       assert.equal(null, err);
       console.log('Item inserted');
       db.close();
