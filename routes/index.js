d45b679bfb86a4489c39af31a1cca53d0a5f809a
SunilSigar <sunilsigar001@gmail.com>
2017-12-11 20:34:03 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var mongo = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/bestjockes';
+var url = 'mongodb://sunilsigar:vfr4m5tgb@ds135946.mlab.com:35946/jokes';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
